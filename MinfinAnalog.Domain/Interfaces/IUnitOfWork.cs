874361ca77c3ad303aca5eca5c3d1880e543e961
DESKTOP-8O3GQHQ\serji <serjickua@gmail.com>
2021-10-20 15:16:16 +0300

    Add user serivce.

---
+++ MinfinAnalog.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinfinAnalog.Domain.Interfaces
+{
+    interface IUnitOfWork
+    {
+        int Save();
+    }
+}
---
+++ MinfinAnalog.Domain/Interfaces/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using MinfinAnalog.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinfinAnalog.Domain.Interfaces
+{
+    interface IUserService
+    {
+            Task<IEnumerable<UserDTO>> GetUserList();
+            Task<UserDTO> GetUserById(int userId);
+            Task<UserDTO> Create(UserDTO userModel);
+            Task Update(UserDTO userModel);
+            Task Delete(UserDTO userModel);
+    }
+}
